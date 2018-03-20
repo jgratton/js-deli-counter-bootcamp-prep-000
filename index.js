64d9a06de97a0a74e10e22c13e1da70a3d10de3f
John Gratton <jgratton@gmail.com>
2018-03-20 01:45:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function currentLine(line) {
   } else {
     var array = [];
     for (let i = 1; i < line.length; i++) {
-      array.push(`${i}. ${line[i--]}`);
+      array.push(`${i}. ${line[i]}`);
     }
     return `The line is currently: ${array}`;
   }
