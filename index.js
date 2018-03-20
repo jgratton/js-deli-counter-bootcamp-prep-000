b27399bb7f428e40165001c738cfe0595fa22a72
John Gratton <jgratton@gmail.com>
2018-03-20 01:21:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function currentLine(line) {
   if (line.length === 0) {
     return 'The line is currently empty.'
   } else {
-    for 
+    for (let i = 0, )
     return `The line is currently: `;
   }
 }
