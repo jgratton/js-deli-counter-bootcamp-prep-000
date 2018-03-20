ed29cce4e79b5bfd50d7c6396a2d885bf1dfb628
John Gratton <jgratton@gmail.com>
2018-03-20 01:01:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
-  
+  if (katzDeliLine.length === 0) {
+    katzDeliLine.push(name);
+    return `Welcome, ${name}. You are number 1 in line.`;
+  } else {
+    katzDeliLine.push(name);
+    return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+  }
 }
