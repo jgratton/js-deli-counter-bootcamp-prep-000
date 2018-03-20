533876807293d8d678fdf69695a6a600fc67bd74
John Gratton <jgratton@gmail.com>
2018-03-20 01:08:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ function takeANumber(katzDeliLine, name) {
     return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
   }
 }
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return 'The line is empty.';
+  } else {
+    return `The line is serving ${katzDeliLine[1]}.`
+    katzDeliLine.shift();
+  }
+}
