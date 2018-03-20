9e4738441e6fcb29b59dd9d5c4b43b810b3855ff
John Gratton <jgratton@gmail.com>
2018-03-20 01:09:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function takeANumber(katzDeliLine, name) {
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
-    return 'The line is empty.';
+    return 'Theere is nobody waiting to be served!';
   } else {
     return `The line is serving ${katzDeliLine[1]}.`
     katzDeliLine.shift();
